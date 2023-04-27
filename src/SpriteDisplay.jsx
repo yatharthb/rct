a1a08ae2acc76c9bb8233e776b073592cb67949a
yatharthb <2410255-yatharthb@users.noreply.replit.com>
2023-04-27 15:30:19 +0000

    Sprite sheet frames added

--- src/SpriteDisplay.jsx
+++ src/SpriteDisplay.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 const SpriteDisplay = () => {
+  const frameWidth = 20;
+  const frameHeight = 20;
+  const halfWidth = 119; // Half the width of the sprite sheet
+
+  const frameStyle = (xOffset) => ({
+    width: `${frameWidth}px`,
+    height: `${frameHeight}px`,
+    overflow: 'hidden',
+    display: 'inline-block',
+    margin: '5px',
+    position: 'relative',
+  });
+
+  const imgStyle = (xOffset) => ({
+    position: 'absolute',
+    left: `${-xOffset}px`,
+  });
+
   const spriteSheetUrl = '/images/spritesheet.png'; // Use the direct URL in the images folder
 
   return (
     <div>
-      <img src={spriteSheetUrl} alt="Runner sprite sheet" />
+      <div style={frameStyle(0)}>
+        <img src={spriteSheetUrl} alt="frame 1" style={imgStyle(0)} />
+      </div>
+      <div style={frameStyle(halfWidth)}>
+        <img src={spriteSheetUrl} alt="frame 2" style={imgStyle(halfWidth)} />
+      </div>
     </div>
   );
 };
