4fe31f4fec7c1d5aa59563402f984dab68690f1d
yatharthb <51863781+yatharthb@users.noreply.github.com>
2023-04-27 21:08:56 +0000

    Phone support for harmonograph

--- src/Harmonograph/HarmonographSimulator.jsx
+++ src/Harmonograph/HarmonographSimulator.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import Slider from 'react-input-slider'; // Import the Slider component
+import './HarmonographSimulator.css'; // Import the CSS file
 
 const HarmonographSimulator = () => {
   const canvasRef = useRef(null);
@@ -7,6 +8,9 @@ const HarmonographSimulator = () => {
     a1: 150,
     f1: 3,
     f2: 2,
+    a2: 100,
+    f3: 4,
+    f4: 3,
   });
 
   useEffect(() => {
@@ -19,12 +23,12 @@ const HarmonographSimulator = () => {
       const x =
         centerX +
         params.a1 * Math.sin(params.f1 * t + Math.PI / 16) * Math.exp(-0.02 * t) +
-        params.a1 * Math.sin(params.f2 * t + (3 * Math.PI) / 2) * Math.exp(-0.01 * t);
+        params.a2 * Math.sin(params.f2 * t + (3 * Math.PI) / 2) * Math.exp(-0.01 * t);
 
       const y =
         centerY +
         params.a1 * Math.cos(params.f1 * t + Math.PI / 16) * Math.exp(-0.02 * t) +
-        params.a1 * Math.cos(params.f2 * t + (3 * Math.PI) / 2) * Math.exp(-0.01 * t);
+        params.a2 * Math.cos(params.f2 * t + (3 * Math.PI) / 2) * Math.exp(-0.01 * t);
 
       ctx.fillStyle = 'black';
       ctx.fillRect(x, y, 1, 1);
@@ -51,31 +55,29 @@ const HarmonographSimulator = () => {
   return (
     <div>
       <h2>Harmonograph Simulator</h2>
-      <div>
+      <div className="sliders-container">
         {Object.keys(params).map((key) => (
-          <div key={key}>
-            <label>
-              {key}:
-              <Slider
-                axis="x"
-                x={params[key]}
-                onChange={({ x }) => updateParams(key, x)}
-                styles={{
-                  track: {
-                    backgroundColor: '#ddd',
-                  },
-                  active: {
-                    backgroundColor: '#5fcf80',
-                  },
-                  thumb: {
-                    width: 20,
-                    height: 20,
-                    backgroundColor: '#5fcf80',
-                    borderRadius: 10,
-                  },
-                }}
-              />
-            </label>
+          <div key={key} className="slider-item">
+            <label>{key}:</label>
+            <Slider
+              axis="x"
+              x={params[key]}
+              onChange={({ x }) => updateParams(key, x)}
+              styles={{
+                track: {
+                  backgroundColor: '#ddd',
+                },
+                active: {
+                  backgroundColor: '#5fcf80',
+                },
+                thumb: {
+                  width: 20,
+                  height: 20,
+                  backgroundColor: '#5fcf80',
+                  borderRadius: 10,
+                },
+              }}
+            />
           </div>
         ))}
       </div>
